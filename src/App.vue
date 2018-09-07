12ecc98ab0be6be3cd81dce00cda7bea39d6d61f
Thom David <thomdavid74@gmail.com>
2018-09-07 09:38:02 -0700

    Initial quote component file structure

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,14 @@
 
 <script>
     export default {
-        
+        data: function() {
+            return {
+                quotes: [
+                    'Just a sample quote'
+                ],
+                maxQuotes: 10
+            }
+        }
     }
 </script>
 
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,1 @@
+<!-- Grid of all quotes component -->
